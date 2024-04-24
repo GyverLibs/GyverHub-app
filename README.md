042416c1658875d8f79a04bd07cd7c6a2b103e89
AlexGyver <alexgyvershow@ya.ru>
2024-04-24 15:47:17 +0300

    upd

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# gyver_hub
+# GyverHub-app
 
-Панель управления для esp8266, esp32 и других Arduino. Конструктор интерфейса, интеграция в умный дом
+Мобильное приложение для платформы GyverHub
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+> flutter build apk
--- lib/env.dart
+++ lib/env.dart
@@ -1,16 +1,14 @@
-
-
-
 import 'package:flutter/material.dart';
 
-class Env{
+class Env {
+  // release
   static const versionUrl = "https://github.com/GyverLibs/GyverHub-web/releases/latest/download/version.txt";
   static const hubUrl = "https://github.com/GyverLibs/GyverHub-web/releases/latest/download/GyverHub.html";
 
-  // static const hubUrl = "https://raw.githubusercontent.com/GyverLibs/GyverHub-web/main/app/index.html";
-
+  // dev
+  // static const versionUrl = "https://raw.githubusercontent.com/GyverLibs/GyverHub-web/main/dist/version.txt";
+  // static const hubUrl = "https://raw.githubusercontent.com/GyverLibs/GyverHub-web/main/dist/GyverHub.html";
 
   static const primColor = Color(0xFF37a93c);
   static const bgColorDark = Color(0xFF26272c);
-
 }
