708df13e09f571c912604ef1908aa042ba1cb028
Егор Шепелев <egorshepelev@MacBook-Pro-Egor.local>
2024-05-16 00:39:16 +0300

    restore env consts

--- lib/core/env.dart
+++ lib/core/env.dart
@@ -1,26 +1,15 @@
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:gyver_hub/core/enums.dart';
 
 class Env {
-  // change this value to switch env
-  static const buildMode = BuildMode.dev;
+  // release
+  // static const versionUrl = "https://github.com/GyverLibs/GyverHub-web/releases/latest/download/version.txt";
+  // static const hubUrl = "https://github.com/GyverLibs/GyverHub-web/releases/latest/download/GyverHub.html";
 
-  static String get versionUrl => buildMode == BuildMode.release
-      ? _Consts.releaseVersionUrl
-      : _Consts.devVersionUrl;
-  static String get hubUrl => buildMode == BuildMode.release
-      ? _Consts.releaseHubUrl
-      : _Consts.devHubUrl;
-  static WebUri localServerUri = WebUri('http://localhost:9090/');
-}
-
-class _Consts {
-  static const releaseVersionUrl =
-      'https://github.com/GyverLibs/GyverHub-web/releases/latest/download/version.txt';
-  static const releaseHubUrl =
-      'https://github.com/GyverLibs/GyverHub-web/releases/latest/download/GyverHub.html';
-  static const devVersionUrl =
+  // dev
+  static const versionUrl =
       'https://raw.githubusercontent.com/GyverLibs/GyverHub-web/main/dist/version.txt';
-  static const devHubUrl =
+  static const hubUrl =
       'https://raw.githubusercontent.com/GyverLibs/GyverHub-web/main/dist/GyverHub.html';
+
+  static WebUri localServerUri = WebUri('http://localhost:9090/');
 }
